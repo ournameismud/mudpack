9aa02ebab7dadc59ba5968dd10412e50cbdd69d3
dhmax88 <dhmax88@gmail.com>
2018-11-04 17:42:53 +0000

    new

---
+++ config/twigpack.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    // Global settings
+    '*' => [
+        // If `devMode` is on, use webpack-dev-server to all for HMR (hot module reloading)
+        'useDevServer' => false,
+        // The JavaScript entry from the manifest.json to inject on Twig error pages
+        'errorEntry' => '',
+        // Manifest file names
+        'manifest' => [
+            'legacy' => 'manifest-legacy.json',
+            'modern' => 'manifest.json',
+        ],
+        // Public server config
+        'server' => [
+            'manifestPath' => '/dist/',
+            'publicPath' => '/',
+        ],
+        // webpack-dev-server config
+        'devServer' => [
+            'manifestPath' => 'http://localhost:8080/',
+            'publicPath' => 'http://localhost:8080/',
+        ],
+        // Local files config
+        'localFiles' => [
+            'basePath' => '@webroot/',
+            'criticalPrefix' => 'dist/criticalcss/',
+            'criticalSuffix' => '_critical.min.css',
+        ],
+    ],
+    // Live (production) environment
+    'live' => [
+    ],
+    // Staging (pre-production) environment
+    'staging' => [
+    ],
+    // Local (development) environment
+    'local' => [
+        // If `devMode` is on, use webpack-dev-server to all for HMR (hot module reloading)
+        'useDevServer' => true,
+        // The JavaScript entry from the manifest.json to inject on Twig error pages
+        'errorEntry' => 'app.js',
+        // webpack-dev-server config
+        'devServer' => [
+            'manifestPath' => 'http://localhost:8080/',
+            'publicPath' => 'http://127.0.0.1:8080/',
+        ],
+    ],
+];
